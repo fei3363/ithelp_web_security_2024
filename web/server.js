db6597ae93f1d0e19879c69920021346c3850be7
fei3363 <feifei3363@gmail.com>
2024-09-16 05:11:44 +0000

    Day2: 了解使用者輸入與輸出與前端關係

--- web/server.js
+++ web/server.js
@@ -13,11 +13,42 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// 設定 form
+app.get('/form', (req, res) => {
+  // 當存取 form 路徑時，請求 form.html 檔案
+  res.sendFile(path.join(__dirname, 'public', 'form.html'));
+});
+
+
 // 設定 API 路由
 app.get('/api/hello', (req, res) => {
   // 當存取 /api/hello 路徑時，回傳 JSON 
   res.json({ message: 'Hello from the server!' });
 });
+
+// 設定搜尋路由
+app.get('/search', (req, res) => {
+  // 取得查詢字串參數
+  const keyword = req.query.keyword;
+  // 顯示查詢字串參數
+  res.send(`你搜尋的關鍵字是：${keyword}`);
+});
+
+// 使用 express.json() 來解析 JSON 格式的請求
+app.use(express.json());
+
+// 使用 express.urlencoded() 來解析 URL 編碼的請求
+app.use(express.urlencoded({ extended: true }));
+
+// 設定表單提交路由
+app.post('/submit', (req, res) => {
+  // 取得 POST 請求的參數
+  const { username, password } = req.body;
+  // 顯示 POST 請求的參數
+  res.send(`你的帳號是 ${username}，密碼是 ${password}`);
+});
+
+
 
 // 啟動伺服器
 app.listen(port, () => {
